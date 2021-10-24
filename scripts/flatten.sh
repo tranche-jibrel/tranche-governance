ab2e245636219bf65f3349483af6858e7a2c828b
Saurabh <sw@tranche.finance>
2021-10-24 10:29:50 +0400

    added token to flatten.sh

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -14,3 +14,6 @@ npx truffle-flattener ./contracts/SliceGovernor.sol | awk '/SPDX-License-Identif
 
 echo "Flattening # TimeLock contract"
 npx truffle-flattener ./contracts/Timelock.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next} 1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/Timelock.sol
+
+echo "Flattening # Token contract"
+npx truffle-flattener ./contracts/Token.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next} 1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/Token.sol
